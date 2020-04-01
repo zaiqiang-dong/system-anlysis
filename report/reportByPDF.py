0986fc9ff99267b3ced65f85dd0c9504d365a7ab
Zaiqiang dong <zqdong@nreal.ai>
2020-04-01 11:09:23 +0800

    For complie pdf perfect

--- report/reportByPDF.py
+++ report/reportByPDF.py
@@ -7,6 +7,7 @@ import os
 import math
 from reportlab.pdfgen import canvas
 import pandas as pd
+import random
 
 (w, h) = portrait(A0)
 headStr=''
@@ -52,7 +53,7 @@ def processRy(c, ry):
         reportDrawHead(c)
         return h
 def createIndext(c, p):
-    lv = 0
+    lv = -1
     sty = p.style.name
     if sty == "Heading1":
         lv = 0
@@ -60,10 +61,12 @@ def createIndext(c, p):
         lv = 1
     elif sty == "Heading3":
         lv = 2
-    key = str(hash(p))
-    txt = p.getPlainText()
-    c.bookmarkPage(key)
-    c.addOutlineEntry(txt, key, level=lv, closed=0)
+        closeValue = 1
+    if lv != -1:
+        key = str(hash(p)) + str(random.random())
+        tex = p.getPlainText()
+        c.bookmarkPage(key)
+        c.addOutlineEntry(tex, key, level=lv, closed=0)
 
 def reportDrawHead(c):
     #head
@@ -106,53 +109,39 @@ def reportDrawHead(c):
 
 def reportDrawH1(c,name, y = 0):
 
-    hp = Paragraph(name, h1)
-    whp,hhp=hp.wrap(1000,1000)
+    hp1 = Paragraph(name, h1)
+    whp,hhp=hp1.wrap(2000,1000)
 
     ry = h - paidingTop - hhp  - contentPaiding
 
-    hp.drawOn(c, paidingLeft, ry)
-    allParagraph.append(hp)
+    hp1.drawOn(c, paidingLeft, ry)
 
-    # key = str(hash(hp))
-    # txt = name
-    # c.bookmarkPage(key)
-    # c.addOutlineEntry(txt, key, level=0, closed=0)
-    createIndext(c, hp)
+    createIndext(c, hp1)
     return processRy(c,ry)
 
 def reportDrawH2(c,name, y):
 
-    hp = Paragraph("&nbsp;&nbsp;&nbsp;&nbsp;" + name, h2)
-    whp,hhp=hp.wrap(1000,1000)
+    hp2 = Paragraph( name, h2)
+    whp,hhp=hp2.wrap(2000,1000)
 
     ry = y - hhp - contentPaiding
-    hp.drawOn(c, paidingLeft, ry)
+    hp2.drawOn(c, paidingLeft, ry)
 
-    createIndext(c, hp)
-    # key = str(hash(hp))
-    # txt = name
-    # c.bookmarkPage(key)
-    # c.addOutlineEntry(txt, key, level=1, closed=0)
+    createIndext(c, hp2)
 
     return processRy(c,ry)
 
 def reportDrawH3(c,name, y):
-
-    hp = Paragraph("&nbsp;&nbsp;&nbsp;&nbsp;" + name, h3)
-    whp,hhp=hp.wrap(2000,0)
+    hp3 = Paragraph(name, h3)
+    whp,hhp=hp3.wrap(2000,1000)
     if y - hhp - paidingBottom - contentPaiding*2 < 0:
         c.showPage()
         reportDrawHead(c)
         y = h - contentPaiding
     ry = y - hhp - contentPaiding * 2
-    hp.drawOn(c, paidingLeft, ry)
+    hp3.drawOn(c, paidingLeft, ry)
 
-    createIndext(c, hp)
-    # key = str(hash(hp))
-    # txt = name
-    # c.bookmarkPage(key)
-    # c.addOutlineEntry(txt, key, level=2, closed=0)
+    createIndext(c, hp3)
 
     return processRy(c,ry)
 
@@ -178,12 +167,15 @@ def reportDrawCover(c, CodeVersion, testTime):
     createIndext(c, Paragraph("Cover", h1))
 
 
-def reportDrawTable(c, ifile, ry, colw=None):
+def reportDrawTable(c, ifile, chapter, ry, colw=None):
     styles = getSampleStyleSheet()
     styleN = styles['Normal']
     styleN.wordWrap = 'CJK'
     styleN.fontSize = 24
     styleN.leading = 40
+
+    txt = ifile.split('/')[-1].split('.')[0].strip()
+    ry = reportDrawH3(c, chapter + txt, ry)
 
     df = pd.read_csv(ifile)
     cols = df.columns.tolist()
@@ -191,56 +183,97 @@ def reportDrawTable(c, ifile, ry, colw=None):
     data.insert(0, cols)
 
     tableStyle = [
+        ('BACKGROUND',(0,0),(-1,0), colorG),
+        ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+        ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+        ]
+    tableStyleHead = [
+        ('BACKGROUND',(0,0),(-1,0), colorG),
         ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
         ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
         ]
 
-    data = [[Paragraph(str(cell), styleN) for cell in row] for row in data]
+    styleN.fontSize = 36
+    styleN.leading = 48
+    data1 = [[Paragraph(str(cell), styleN) for cell in row] for row in data[0:1]]
+    styleN.fontSize = 24
+    styleN.leading = 40
+    data2 = [[Paragraph(str(cell), styleN) for cell in row] for row in data[1:]]
+    data = data1 + data2
 
     t = Table(data, colWidths=colw)
     wt, ht = t.wrap(0, 0)
-    remainY = ry - paidingBottom - ht
+    remainY = ry - contentPaiding - paidingBottom - ht
 
     datap= []
-    dataa= []
+    FirstPartTable= True
 
-    needSplit = False
-
-    if remainY < 0:
+    while remainY < 0:
         remainY = -remainY
         p = remainY / ht
         lendata = len(data)
-        splitP = math.floor(lendata * p)
-        datap = data[0:p]
-        dataa = data[p:]
-        needSplit = True
+        splitP = math.ceil(lendata * p)
+        splitP = lendata - splitP
+        datap = data[0:splitP]
+        data = data1 + data[splitP:]
 
-    if not needSplit:
-        print(" table no need split.")
-        t.setStyle(TableStyle(tableStyle))
-        t.split(0,0)
-        t.drawOn(c, paidingLeft , ry - contentPaiding - ht - paidingTop)
-        return processRy(c,ry)
-    else:
         t = Table(datap, colWidths=colw)
         t.setStyle(TableStyle(tableStyle))
         t.split(0,0)
         pw, ph = t.wrap(0, 0)
-        t.drawOn(c, paidingLeft , ry - ph)
+        t.drawOn(c, paidingLeft , ry - ph - contentPaiding)
+        c.showPage()
+        reportDrawHead(c)
 
-        t = Table(dataa, colWidths=colw)
-        t.setStyle(TableStyle(tableStyle))
-        t.split(0,0)
-        aw, ah = t.wrap(0, 0)
-        ry = h - ah - paidingTop - contentPaiding
-        t.drawOn(c, paidingLeft , ry)
+        ry = h - contentPaiding
+        t = Table(data, colWidths=colw)
+        wt, ht = t.wrap(0, 0)
+        remainY = ry - paidingBottom - contentPaiding - ht
 
-        return processRy(c,ry)
+    t = Table(data, colWidths=colw)
+    wt, ht = t.wrap(0, 0)
+    t.setStyle(TableStyle(tableStyle))
+    t.split(0,0)
+    t.drawOn(c, paidingLeft , ry - contentPaiding - ht)
+    return processRy(c,ry)
 
 
-def reportDrawImage(c,path, imgW, imgH, y):
-    txt = path.split('/')[-1].split('.')[0]
-    ry = reportDrawH3(c, txt, y)
+    # needSplit = False
+
+    # if remainY < 0:
+    #     remainY = -remainY
+    #     p = remainY / ht
+    #     lendata = len(data)
+    #     splitP = math.floor(lendata * p)
+    #     datap = data[0:splitP]
+    #     data = data[splitP:]
+    #     needSplit = True
+
+    # if not needSplit:
+    #     t.setStyle(TableStyle(tableStyle))
+    #     t.split(0,0)
+    #     t.drawOn(c, paidingLeft , ry - contentPaiding - ht - paidingTop)
+    #     return processRy(c,ry)
+    # else:
+    #     t = Table(datap, colWidths=colw)
+    #     t.setStyle(TableStyle(tableStyle))
+    #     t.split(0,0)
+    #     pw, ph = t.wrap(0, 0)
+    #     t.drawOn(c, paidingLeft , ry - ph)
+
+    #     t = Table(dataa, colWidths=colw)
+    #     t.setStyle(TableStyle(tableStyle))
+    #     t.split(0,0)
+    #     aw, ah = t.wrap(0, 0)
+    #     ry = h - ah - paidingTop - contentPaiding
+    #     t.drawOn(c, paidingLeft , ry)
+
+    #     return processRy(c,ry)
+
+
+def reportDrawImage(c,path, chapter, imgW, imgH, ry):
+    txt = path.split('/')[-1].split('.')[0].strip()
+    ry = reportDrawH3(c, chapter + txt, ry)
     ry = ry - imgH - paidingBottom
     if ry <= 0:
         c.showPage()
@@ -251,15 +284,14 @@ def reportDrawImage(c,path, imgW, imgH, y):
 
 
 
-def reportDrawMemory(c,img_path, imgW, imgH, y):
+def reportDrawMemory(c,img_path, chapter, imgW, imgH, y):
     pages = 0
     l = os.listdir(img_path)
     i = 0
     ry = y
     while i < len(l):
         f = img_path + "/"+  str(l[i])
-        print(f)
-        ry = reportDrawImage(c, f, imgW, imgH, ry)
+        ry = reportDrawImage(c, f, chapter + "." + str(i + 1) + " ", imgW, imgH, ry)
         i += 1
     return ry
 
@@ -274,12 +306,10 @@ def createReport(outdir, testVersion, testTime):
 
     l = os.listdir(outdir + "/process-loginfo/")
     logcatResult = outdir+"/process-loginfo/"+l[0]
-    print(logcatResult)
     reportDrawHead(c)
     ry = reportDrawH1(c, "1 Logcat Log Analyst")
     ry = reportDrawH2(c, "1.1 Analyst out", ry)
-    print(logcatResult)
-    reportDrawTable(c, str(logcatResult), ry, colw=[100,1500, 200,120])
+    ry = reportDrawTable(c, str(logcatResult),"1.1.1 ", ry, colw=[100,1400, 300,120])
     c.showPage()
 
     memoryResultProcessPath = outdir + "/process-memory-info/"
@@ -289,40 +319,20 @@ def createReport(outdir, testVersion, testTime):
     reportDrawHead(c)
     ry = reportDrawH1(c, "2 Memory Info Analyst")
     ry = reportDrawH2(c, "2.1 Memory Total Info", ry)
-    ry = reportDrawMemory(c,memoryResultMemPath,1920 , 1200, ry)
+    ry = reportDrawMemory(c,memoryResultMemPath, "2.1",1920 , 1200, ry)
     c.showPage()
     reportDrawHead(c)
     ry = h - paidingTop
     ry = reportDrawH2(c, "2.2 Memory Pss info", ry)
-    ry = reportDrawMemory(c,memoryResultPssPath ,1920 ,720, ry)
+    ry = reportDrawMemory(c,memoryResultPssPath ,"2.2",1920 ,720, ry)
     c.showPage()
     reportDrawHead(c)
     ry = h - paidingTop
     ry = reportDrawH2(c, "2.3 Precess Memory info", ry)
-    reportDrawMemory(c,memoryResultProcessPath,1920 ,640, ry)
+    reportDrawMemory(c,memoryResultProcessPath, "2.3",1920 ,640, ry)
     c.showPage()
     c.save()
 
-    pass
-
 
 if __name__ == "__main__":
-    # c = canvas.Canvas("./adbc.pdf", pagesize = portrait(A0))
-
-    # reportDrawHead(c)
-    # reportDrawCover(c,"NREAL-21231-26724345345", "2020-03-27-19-32-22")
-    # c.showPage()
-    # reportDrawHead(c)
-    # ry = reportDrawH1(c, "1 Logcat Log Analyst")
-    # ry = reportDrawH2(c, "1.1 Analyst out", ry)
-
-    # reportDrawTable(c, "../out/report-2020-03-27-19-32-22/process-loginfo/process-loginfo-2020-03-27-19-34-08.csv", ry, colw=[100,1500, 200,120])
-    # c.showPage()
-    # reportDrawHead(c)
-    # ry = reportDrawH1(c, "2 Memory Info Analyst")
-    # ry = reportDrawH2(c, "2.1 Precess Memory info", ry)
-    # reportDrawMemory(c,"../out/report-2020-03-27-19-32-22/process-memory-info/",1920 ,640, ry)
-    # c.showPage()
-    # c.save()
-
-    createReport("../out/report-2020-03-27-19-32-22", "NREAL-21231-26724345345", "2020-03-27-19-32-22")
+    createReport("../out/report-2020-03-31-15-56-44", "NREAL-21231-26724345345", "2020-03-27-19-32-22")
